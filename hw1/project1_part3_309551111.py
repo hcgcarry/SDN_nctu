fc07714cd3599722118ed269f5a11d2a0b7d0f1e
hcgcarry <hcgcarry@gmail.com>
2021-10-01 10:36:35 +0800

    hw1 finish

---
+++ hw1/project1_part3_309551111.py
@@ -0,0 +1,28 @@
+from mininet.topo import Topo
+
+class project1_Topo_309551111( Topo ):
+    def __init__( self ):
+        Topo.__init__( self )
+
+        # Add hosts
+        h1 = self.addHost( 'h1' ,ip='192.168.0.1/27')
+        h2 = self.addHost( 'h2' ,ip='192.168.0.2/27')
+        h3 = self.addHost( 'h3' ,ip='192.168.0.3/27')
+	#h1.setIP('192.168.0.1/24')
+
+        # Add switches
+        s1 = self.addSwitch( 's1' )
+        s2 = self.addSwitch( 's2' )
+        s3 = self.addSwitch( 's3' )
+        s4 = self.addSwitch( 's4' )
+        
+        # Add links
+        self.addLink( h1, s1 )
+        self.addLink( h2, s2 )
+        self.addLink( h3, s3 )
+        self.addLink( s1, s4 )
+        self.addLink( s2, s4 )
+        self.addLink( s3, s4 )
+
+
+topos = { 'topo_part3_309551111': project1_Topo_309551111}
